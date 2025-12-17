24f720392a179a796fd444cacc5bf78cde270e92
s1ark-qaq <1575628b1t@gmail.com>
2025-12-17 20:50:35 +0800

    1

--- 2.aqi/ws.go
+++ 2.aqi/ws.go
@@ -17,7 +17,7 @@ func main() {
 	//app是AppConfig结构体实例
 	app := aqi.Init(
 		//设置配置文件名
-		aqi.ConfigFile("config-test.yaml"),
+		aqi.ConfigFile("./2.aqi/config-test.yaml"),
 		//服务名称,以及服务端口
 		aqi.HttpServer("my server", "port"),
 	)
@@ -46,7 +46,7 @@ func HandleWs(app *aqi.AppConfig) {
 			"hi": "group1",
 		})
 	})
-	
+
 	app.WithHttpServer(engine)
 	app.Start()
 }
